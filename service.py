adef381dd95bd296e8fbf0e4dafcdc514ca3cb8b
Torsten Martinsen <torsten@bullestock.net>
2021-12-15 18:35:32 +0100

    PoC.

--- service.py
+++ service.py
@@ -1,46 +1,40 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, jsonify
+import logging
+import os
 import requests
 import uuid
 
-from sesamutils.flask import serve
-from sesamutils import sesam_logger, VariablesConfig
-
-
 app = Flask(__name__)
-logger = sesam_logger("Slack command receiver",)
 
-req_vars = ["NODE_JWT", "VALIDATION_TOKEN", "ENDPOINT_URL"]
-config = VariablesConfig(req_vars)
+logger = logging.getLogger('werkzeug')
+handler = logging.FileHandler('acsgw.log')
+logger.addHandler(handler)
+app.logger.addHandler(handler)
 
-if not config.validate():
-    logger.error("Missing required variable(s).")
+def is_request_valid(request):
+    try:
+        is_token_valid = request.form['token'] == os.environ['SLACK_VERIFICATION_TOKEN']
+        is_team_id_valid = request.form['team_id'] == os.environ['SLACK_TEAM_ID']
+    except Exception as e:
+        logger.info("Exception: %s" % e)
+        return False
+    return is_token_valid and is_team_id_valid    
 
-headers = {
-    "Authorization": f"bearer {config.NODE_JWT}"
-}
-
-
-def valid_token(token):
-    return token == config.VALIDATION_TOKEN
-
-
-@app.route("/command", methods=["POST"])
-def command():
-    _id = str(uuid.uuid1())
-    data = dict(request.form)
-    data["_id"] = _id
-    token = data.pop("token")
-    if not valid_token(token):
-        logger.info("Invalid token. Aborting")
+@app.route("/slash/<command>", methods=["POST"])
+def command(command):
+    logger.info("slash")
+    if not is_request_valid(request):
+        logger.info("Invalid request. Aborting")
         return abort(403)
-    logger.info("Valid command received. Pushing to pipe endpoint")
-    resp = requests.post(config.ENDPOINT_URL, json=data, headers=headers)
-    logger.debug(resp.status_code)
-    logger.debug(resp.text)
-    resp.raise_for_status()
+    logger.info("Command received: %s" % command)
+    # TODO
+    return jsonify(
+        response_type='in_channel',
+        text='<https://youtu.be/frszEJb0aOo|General Kenobi!>',
+    )    
     return "", 200
 
 
-# Start the server on port 3000
+# Start the server on port 5000
 if __name__ == "__main__":
-    serve(app)
+    app.run(host='0.0.0.0', port=5000)
