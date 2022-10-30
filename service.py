a66aad1abe59997e715e7f9016fb64bda4cb11e2
Torsten Martinsen <torsten@bullestock.net>
2022-10-30 18:05:29 +0100

    Add bacslog end point.

--- service.py
+++ service.py
@@ -329,7 +329,23 @@ def acslog():
     text = request.json['text']
     day = datetime.datetime.now().strftime("%Y-%m-%d-%H")
     logfilename = '%s/acs-%s.log' % (LOG_DIR, day)
-    logger.info("acslog: logfilename %s" % logfilename)
+    with open(logfilename, 'a+', encoding = 'utf-8') as f:
+        f.write("%s %s\n" % (stamp, text))
+    return "", 200
+
+# /bacslog: Called by BACS to store a log entry
+@app.route("/bacslog", methods=["POST"])
+def acslog():
+    logger.info("bacslog")
+    if not is_acs_request_valid(request):
+        logger.info("Invalid request. Aborting")
+        logger.info("bacslog: request %s" % request.json)
+        return abort(403)
+    stamp = request.json['timestamp']
+    text = request.json['text']
+    day = datetime.datetime.now().strftime("%Y-%m-%d-%H")
+    logfilename = '%s/bacs-%s.log' % (LOG_DIR, day)
+    logger.info("bacslog: logfilename %s" % logfilename)
     with open(logfilename, 'a+', encoding = 'utf-8') as f:
         f.write("%s %s\n" % (stamp, text))
     return "", 200
