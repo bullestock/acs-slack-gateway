3842a06d42d9f0030c1cf1c10d402edacc471dfd
Torsten Martinsen <torsten@bullestock.net>
2026-07-23 17:38:25 +0200

    Add 'latest' tag separately.

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
 serial=`cat docker-serial`
-docker build -t bullestock/acs-gateway:$serial -t bullestock/acs-gateway:latest -f Dockerfile --build-arg GIT_COMMIT=`git rev-parse HEAD` --build-arg VERSION_NUMBER=$serial .
+docker build -t bullestock/acs-gateway:$serial -f Dockerfile --build-arg GIT_COMMIT=`git rev-parse HEAD` --build-arg VERSION_NUMBER=$serial .
+docker tag bullestock/acs-gateway:$serial bullestock/acs-gateway:latest
 docker push bullestock/acs-gateway:$serial
 docker push bullestock/acs-gateway:latest
 expr $serial + 1 > docker-serial
